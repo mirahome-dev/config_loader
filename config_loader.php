77d50cfacf1af3ba01ac5eef291f4834bb7a6db1
zhoubin <zhoubin@mirahome.me>
2017-01-23 16:06:19 +0800

    update

--- config_loader.php
+++ config_loader.php
@@ -3,37 +3,25 @@
 //ini_set('display_errors', '1');
 //require 'vendor/autoload.php';
 
-function config_loader($path = '/mira/api') {
-    global $zk;
-    $zk = new \Kyoz\ZookeeperClient('zk01:2181,zk02:2181,zk03:2181');
-    $env = config_loader_getEnvName();
-    define('ENV', $env);
-    //echo "\n\n执行：$env ".date("Y-m-d H:i:s")."\n";
-    config_loader_loadConfig($path . '/common/const', "const");
-    config_loader_loadConfig($path . '/common/global', "global");
-    config_loader_loadConfig($path . '/'. $env .'/const', "const");
-    config_loader_loadConfig($path . '/'. $env .'/global', "global");
-}
-
-function config_loader_loadConfig($path = '', $type = 'const') {
-    global $zk;
-    $list = @$zk->getChildren($path);
-    if(!empty($list)) {
-        foreach($list as $key) {
-            $value = $zk->get($path . '/' . $key);
-            if($type == 'const') {
-                define("$key", "$value");
-            }else {
-                global $$key;
-                $$key=json_decode("$value", true);
-            }
-        }
+function load_config($path = '/mira', $configs = null) {
+    if(empty($zk)) {
+        $zk = new \Kyoz\ZookeeperClient('zk01:2181,zk02:2181,zk03:2181');
     }
+    if($configs === null) $configs = [];
+    $list = $zk->getChildren($path);
+    if(!empty($list) && is_array($list)) {
+        foreach($list as $key) {
+            $configs[$key] = load_config($path . "/$key");
+        }
+    }else {
+        $configs = $zk->get($path);
+    }
+    return $configs;
 }
 
-function config_loader_getEnvName() {
+function get_env() {
     $eth0Ip = \Kyoz\Utils\Network::getEthIp();
-    global $zk;
+    $zk = new \Kyoz\ZookeeperClient('zk01:2181,zk02:2181,zk03:2181');
     $mapJson = $zk->get('/mira/envEth0Map');
     $map = json_decode($mapJson);
     foreach($map as $env => $ips) {
@@ -45,3 +33,4 @@ function config_loader_getEnvName() {
     }
     return false;
 }
+
